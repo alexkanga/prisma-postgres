aabe4faad394ecbb490e14a7d816da3100ea3d96
alexkanga <kalexane@gmail.com>
2023-11-12 14:07:45 +0000

    add engine prisma client

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const express  = require('express');
+const express = require("express");
 
 const app = express();
-const dotenv = require('dotenv');
- const prisma = require('./db/prisma');
-
-
+const dotenv = require("dotenv");
+const prisma = require("./db/prisma");
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', async (req, res) => {
-    await prisma.user.create({
-        data: {
-            name : "David Crusin",
-            email : "davidcrusin@gmail.com", 
-            password : "motdepasse"  
-        },
-    });
+app.get("/", async (req, res) => {
+  await prisma.user.create({
+    data: {
+      name: "David Crusin",
+      email: "davidcrusin@gmail.com",
+      password: "motdepasse",
+    },
+  });
 
-    //get all users
-    const users = await prisma.user.findMany();
+  //get all users
+  const users = await prisma.user.findMany();
 
-    //get user name
-    const names = users.map((user) => user.name);
-    
+  //get user name
+  const names = users.map((user) => user.name);
 
-    res.send(
-        `There are ${names.length} users with the name ${names.join(', ')}`
-    );
-})
+  res.send(`There are ${names.length} users with the name ${names.join(", ")}`);
+});
 
 app.listen(port, () => {
-    console.log(`App Listening on port ${port}`)
+  console.log(`App Listening on port ${port}`);
 });
